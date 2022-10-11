3a38e6c334b57298c71d5298c44eb230565dc78d
AkiAleksi <aki@hietamaki.org>
2022-10-11 22:25:35 +0300

    ag-grid

--- src/TodoTable.js
+++ src/TodoTable.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import { AgGridReact } from 'ag-grid-react';
 
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
 
 export default function TodoTable(props) {
+    const [columnDefs] = useState([
+        { field: 'desc', sortable: true, filter: true },
+        { field: 'date', sortable: true, filter: true },
+        {
+            field: 'priority', sortable: true, filter: true,
+            cellStyle: params => params.value === "high" ? { color: 'red' } : { color: 'black' }
+        }
+    ])
+    const gridRef = useRef();
+
+    const _deleteRow = () => {
+        if (gridRef.current.getSelectedNodes()[0]) {
+            props.deleteRow(gridRef.current.getSelectedNodes()[0].childIndex)
+        }
+        else {
+            alert('Select row first');
+        }
+    }
 
     if (props.todos.length === 0) {
         return (
@@ -12,25 +33,18 @@ export default function TodoTable(props) {
     } else {
         return (
 
-            <table className='table'>
-                <thead>
-                    <tr><th>Date</th>
-                        <th>Task</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        props.todos.map((todo, idx) => {
-                            return <tr key={idx}><td>{todo.date}</td><td>{todo.desc}</td>
-                                <td><button onClick={() => props.deleteRow(idx)}>Delete</button>
-                                </td>
-                            </tr>
-                        })
-                    }
-                </tbody>
-            </table>
+            <div className="ag-theme-alpine" style={{ height: '700px', width: '50%', margin: 'auto' }} >
+                <AgGridReact
+                    ref={gridRef}
+                    onGridReady={params => gridRef.current = params.api}
+                    rowSelection="single"
+                    columnDefs={columnDefs}
+                    rowData={props.todos}>
+                </AgGridReact>
+                <button onClick={() => _deleteRow()}>Delete</button>
+            </div >
+
         );
     }
-
 
 }
--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import TodoTable from '../TodoTable';
 
 export default function TodoList() {
-    const [newTodo, setNewTodo] = useState({ desc: '', date: '' });
+    const [newTodo, setNewTodo] = useState({ desc: '', date: '', priority: 'low' });
     const [todos, setTodos] = useState([]);
+    
 
     const inputChanged = (event, type) => {
         if (type === 'description') {
@@ -13,14 +14,20 @@ export default function TodoList() {
         if (type === 'date') {
             setNewTodo({ ...newTodo, date: event.target.value })
         }
+
+        if (type === 'priority') {
+            setNewTodo({...newTodo, priority: event.target.value})
+        }
     }
 
     const addTodo = (event) => {
         setTodos([...todos, newTodo]);
     }
 
-    const deleteTodo = (row) => {
-        setTodos(todos.filter((todos, idx) => row !== idx));
+    const deleteTodo = (deleteidx) => {
+        setTodos(todos.filter((todo, index) =>      
+        index !== deleteidx))
+       
     }
 
     return (
@@ -31,10 +38,17 @@ export default function TodoList() {
                 <span className="input-group-text">Date and task</span>
                 <input className="form-control" type="date" onChange={(event) => inputChanged(event, "date")} value={newTodo.date} />
                 <input className="form-control" type="text" onChange={(event) => inputChanged(event, "description")} value={newTodo.desc} />
+                <select value={newTodo.priority} onChange={(event) => inputChanged(event, "priority")}>
+                    <option disabled defaultValue>Priority</option>
+                    <option value="low">Low</option>
+                    <option value="medium">Medium</option>
+                    <option value="high">High</option>
+                </select>
             </div>
             <button onClick={addTodo} type="button" className="btn btn-danger">Add a new task</button>
            
-            <TodoTable deleteRow={deleteTodo} todos={todos}/>
+            <TodoTable deleteRow={deleteTodo} todos={todos} />
+           
         </div>
     );
 };
